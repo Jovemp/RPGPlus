4bd7421404925dcb67b79bab16bb8feaa9cadfa2
rbpaludo <rbpaludo@gmail.com>
2017-11-23 12:02:51 -0200

    update dbIntegration - conexao MySQL e queries

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-let mainWindow = new BrowserWindow({width: 800, height: 600});
+var mainWindow = null;
 var log = console.log;
 
 console.log('alou');
 app.on('ready', function(){
  console.log('alou 2');
-
+ mainWindow = new BrowserWindow({
+   width: 612,
+   height: 384
+ });
  console.log('alou 3');
+ mainWindow.webContents.openDevTools();
  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
  console.log('alou 4');
 })
