e9b57febfb615dce68ccdca625fe2086399869dd
Rodrigo Aguiar <rodrigoaguiar0@gmail.com>
2017-12-10 02:09:50 -0200

    edição de dados de usuário pronta

--- src/app/controller/editProfileController.js
+++ src/app/controller/editProfileController.js
@@ -1,8 +1,9 @@
-document.getElementById('editProfileBtn').addEventListener("click", function() {
+const express = require('express');
+const mysql = require('mysql');
 
-    var name = document.getElementById('editName').value;
-    var email = document.getElementById('editEmail').value;
-    var password = document.getElementById('editPassword').value;
+var email;
+
+function inserirDados () {
 
     var mysql = require("mysql");
 
@@ -31,7 +32,68 @@ document.getElementById('editProfileBtn').addEventListener("click", function() {
     });
 
     // Queries
-    $editQuery = 'UPDATE `user` SET name = `name`, password = `password`, email = `email` WHERE email LIKE `email`';
+    $selectQuery = 'SELECT name, email, password FROM user WHERE email = "'+email+'"';
+
+    connection.query($selectQuery, function(err, rows, fields) {
+
+        if(err){
+            return console.log("An error ocurred with the query", err);
+        }
+        document.getElementById('editName').value = rows[0].name;
+        document.getElementById('editEmail').value = rows[0].email;
+        document.getElementById('editPassword').value = rows[0].password;
+        console.log(rows);
+        console.log('Inserted ' + rows.affectedRows + ' rows');
+        console.log("Entrei na query");
+        successEdit = true;
+        console.log("Executou a Query");
+    });
+
+    connection.end(function() {
+        console.log("Connection succesfully closed");
+        if (successEdit){
+        }
+    });
+}
+
+document.getElementById('loginBtn').addEventListener("click", function(){
+    email = document.getElementById('inputEmail').value;
+});
+
+document.getElementById('editProfileBtn').addEventListener("click", function() {
+
+    var nameEdit = document.getElementById('editName').value;
+    var emailEdit = document.getElementById('editEmail').value;
+    var passwordEdit = document.getElementById('editPassword').value;
+
+    var mysql = require("mysql");
+
+    var successEdit = false;
+
+    console.log("Modificou as variáveis de nome, email e senha");
+
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "rpgplus"
+    });
+
+    console.log("Criou a conexão");
+
+    connection.connect(function(err) {
+        if(err){
+            console.log(err.stack);
+            console.log("Não conectou");
+            return console.log(err.stack);
+        }
+
+        console.log("Conectou");
+    });
+
+    // Queries
+    $editQuery = 'UPDATE `user` SET name = "'+nameEdit+'", password = "'+passwordEdit+'", email = "'+emailEdit+'" WHERE email = "'+email+'"';
 
     connection.query($editQuery, function(err, rows, fields) {
 
@@ -44,7 +106,6 @@ document.getElementById('editProfileBtn').addEventListener("click", function() {
         successEdit = true;
         console.log("Executou a Query");
     });
-
 
 
     connection.end(function() {
