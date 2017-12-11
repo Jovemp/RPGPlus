8b1f869571cd2b66878d9bedf0f639b122f776b3
Newton <newtoljunior@hotmail.com>
2017-12-11 18:00:46 -0200

    Arquivo de teste
    Rolagem de dados

---
+++ src/test/testRolagemDados.js
@@ -0,0 +1,11 @@
+const assert = require ('chai').assert;
+const editQuery = require ('../rpgplus/src/app/views/diceRollingView');
+
+
+  it('valor', function(){
+    assert.typeOf(value,'integer')
+  });
+  
+  it('soma', function(){
+    assert.typeOf(sum,'integer')
+  });
